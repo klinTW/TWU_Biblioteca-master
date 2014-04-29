550d16e86b66c34e259253c36cf035acc31cb849
k80nocturne <k80vengeance@gmail.com>
2014-04-29 13:32:38 -0500

    Welcome message works

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -2,17 +2,21 @@ package com.twu.biblioteca;
 
 import org.junit.*;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Created by kelin on 4/29/14.
  */
 public class BibliotecaTest {
     @Test
-    public void test() {
+    public void testReturnsCorrectListOfBooks() {
         Librarian testLib = new Librarian();
         assertArrayEquals(testLib.getBooks(),new String[]{"A Tale of Two Cities"});
+    }
 
+    @Test
+    public void testReturnsCorrectWelcomeMessage() {
+        BibliotecaApp testApp = new BibliotecaApp();
+        assertEquals(testApp.welcome(), "Welcome to Bangalore Public Library!");
     }
 }
