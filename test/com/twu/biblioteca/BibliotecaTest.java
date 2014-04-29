ab6633d207cc43d1b6e5beaa7ffe2c1f59fc4f2d
k80nocturne <k80vengeance@gmail.com>
2014-04-29 14:25:17 -0500

    Refactorin'

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -4,19 +4,23 @@ import org.junit.*;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by kelin on 4/29/14.
- */
 public class BibliotecaTest {
+
     @Test
-    public void testReturnsCorrectListOfBooks() {
-        Librarian testLib = new Librarian();
-        assertArrayEquals(testLib.getBooks(),new String[]{"A Tale of Two Cities"});
+    public void testReturnsSingleBook() {
+        BibliotecaApp testApp = new BibliotecaApp();
+        String testBook= "A Tale of Two Cities";
+        assertEquals(testApp.getBooks(), testBook);
     }
 
     @Test
     public void testReturnsCorrectWelcomeMessage() {
         BibliotecaApp testApp = new BibliotecaApp();
         assertEquals(testApp.welcome(), "Welcome to Bangalore Public Library!");
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
     }
 }
